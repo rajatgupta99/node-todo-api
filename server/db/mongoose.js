e7a1ac2b3eb046279d627917c5794b8512746545
Rajat Gupta Thinkpad X1 <rajatgupta99@gmail.com>
2019-02-01 23:03:34 -0800

    v4

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-var uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+var uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`${uri}/TodoApp`, {useNewUrlParser: true});
+mongoose.connect(`${uri}`, {useNewUrlParser: true});
 
 module.exports = {
   mongoose
